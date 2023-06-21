cf1ff89aaa792bab2bfe6c1ba62859f78e778e8c
Jonathan Hernandez <pgr.gni.20@gmail.com>
2023-06-21 10:37:47 -0600

    Componente de CARD

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,12 @@
 
+import Card from '../Components/Card';
 import Layout from '../Components/Layout';
 
 function Home() {
   return (
     <Layout>
       Home 🏠
+      <Card/>
     </Layout>
   )
 }
