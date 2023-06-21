013bca8942cccf279b11fe72fdf356080057fffd
Jonathan Hernandez <pgr.gni.20@gmail.com>
2023-06-21 12:44:20 -0600

    Consumo de API

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,26 @@
 
+import { useState, useEffect } from 'react';
 import Card from '../Components/Card';
 import Layout from '../Components/Layout';
 
 function Home() {
+  const [items, setItems] = useState(null);
+  useEffect(() => {
+    fetch('https://api.escuelajs.co/api/v1/products')
+      .then(res => res.json())
+      .then(data => setItems(data))
+  }, [])
+
   return (
     <Layout>
-      Home 🏠
-      <Card/>
+      <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
+        {
+          items?.map(item => (<Card 
+            key={ item.id }
+            data={ item }
+            />))
+        }
+      </div>
     </Layout>
   )
 }
